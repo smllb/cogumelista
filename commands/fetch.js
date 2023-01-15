5b0e4f43af7cca91f7ab22f47d382260e095bc2f
smllb <samuelbarbosa.ipms@gmail.com>
2023-01-15 01:13:24 -0300

    fetch

--- commands/fetch.js
+++ commands/fetch.js
@@ -8,11 +8,28 @@ const { DataResolver } = require('discord.js');
 const { SlashCommandBuilder } = require('discord.js');
 const path = require('path');
 
+
+buildQueryUrl = (baseURL, pageToken) => {
+    const buildUrl = {
+        base:  baseURL,
+        token: `&pageToken=${pageToken}`
+    }
+
+    if (pageToken) {
+        
+        return buildUrl.base+buildUrl.token
+    } 
+    return buildUrl.base;
+    
+}
+
 async function retrieveDataFromQuery(baseURL, pageToken) {
     let reg = /\||\\|\/|:|\\?|"|<|>/ig;
-
-    return fetch(`${baseURL}&pageToken${pageToken}`)
-        .then(console.log(`Fetching: ${baseURL}&pageToken${pageToken}`))
+    
+    let url = buildQueryUrl(baseURL, pageToken)
+    
+    return fetch(`${url}`)
+        .then(console.log(`sFetching: ${url}}`))
         .then(response => response.json())
         .then(data =>  {
              const items = data.items
@@ -20,7 +37,6 @@ async function retrieveDataFromQuery(baseURL, pageToken) {
                 .map((item, i)=> ({
                     id: item.snippet.resourceId.videoId,
                     title: item.snippet.title.replace(reg, "")
-                
                 }))   
                 return {
                     items: items,
@@ -29,36 +45,81 @@ async function retrieveDataFromQuery(baseURL, pageToken) {
         })    
     } 
 
+    async function downloadAudio (videoId, videoTitle, interaction) {
+ 
+        const oggString = `yt-dlp -f bestaudio --extract-audio --audio-format vorbis --audio-quality 5 -o F:/bot/music/${videoTitle} ${videoId}`
+        //const oggString = `yt-dlp -f bestaudio --extract-audio --audio-format vorbis --audio-quality 5 -o F:/bot/music/"%(title)s.%(ext)s" ${videoId}`
+        
+        
+        
+        const child = exec(oggString, (err, res) => {
+          console.log(`string: ${oggString} | ${videoTitle}\n`);
+          if (err) return console.log(err);
+        
+        })
+        const result = new Promise(resolve => {
+        
+          child.on('exit', () => {
+            console.log(`Download finished at: F:/bot/music/${videoTitle}.ogg`)
+            interaction.channel.send({ 
+                content: `Download finished, uploading file at F:/bot/music/${videoTitle}.ogg`,
+                files: [
+                  `F:/bot/music/${videoTitle}.ogg`,
+                ]
+              });
+              
+            resolve()
+        });
+        })
+        return result;
+        
+        }
+
 
 
 
 async function retrievePlaylistObject (playlistID) {
 
-    const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?` +
+    //const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?` +
     `part=snippet,status` +
-    `&fields=items(status(privacyStatus),snippet(title,resourceId)), nextPageToken` +
+    `&fields=items(status(privacyStatus),snippet(title,resourceId)),nextPageToken` +
     `&playlistId=${playlistID}&maxResults=50&key=${apiKey}`;
+    const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,status&fields=items(status(privacyStatus),snippet(title,resourceId)),nextPageToken&playlistId=${playlistID}&maxResults=50&key=${apiKey}`;
 
 
     let query = await retrieveDataFromQuery(baseURL);
     const playlist = {};
+    playlist.items = query.items;
     playlist.tokens = [query.nextPageToken];
+    let nextIndex = playlist.tokens.length;
+    playlist.tokens[nextIndex] = 'yeet';
+    nextPageToken = playlist.tokens[0]
+    //console.log(nextPageToken)
+    
+    
+    console.log("test: " + playlist.tokens.length)
+    if (playlist.tokens[0]) {
+        console.log(`Token found ${nextPageToken}`)
 
-    console.log(query.nextPageToken)
-    if (query.nextPageToken) {
-        console.log(`Token found ${playlist.tokens[0]}`)
-        
-        while (query.nextPageToken) {
-
+        while (nextPageToken) {
+           // console.log(`Entering with token: ${playlist.tokens[nextIndex]}`)
+            let nextQuery = await retrieveDataFromQuery(baseURL, nextPageToken);
+            playlist.tokens[nextIndex] = nextQuery.nextPageToken
+            nextPageToken = playlist.tokens[nextIndex]
+            //console.log(nextPageToken)
+            console.log(`Leaving with token: ${nextQuery.nextPageToken}`)
+            //console.log(nextQuery)
+            
         }
     } else {
+       console.log(`No token found ${playlist.tokens[0]}`)
         // downloadVideoAndConvert()
 
     }
 
 }
 
-//retrievePlaylistObject('PL4fGSI1pDJn4Gs2meaJRo9O8PNYvhjHIg')
+retrievePlaylistObject('PLpuDUpB0osJmZQ0a3n6imXirSu0QAZIqF')
 
 
 
